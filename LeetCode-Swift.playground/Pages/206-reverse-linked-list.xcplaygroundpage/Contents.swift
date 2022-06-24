3d8319d90cf99629c085f5a3c3359386a33378d6
EvenLin <evenlinyf@gmail.com>
2022-06-24 12:06:07 +0800

    reverse-linked-list

--- LeetCode-Swift.playground/Pages/206-reverse-linked-list.xcplaygroundpage/Contents.swift
+++ LeetCode-Swift.playground/Pages/206-reverse-linked-list.xcplaygroundpage/Contents.swift
@@ -1,4 +1,5 @@
 //: [Previous](@previous)
+// https://leetcode.cn/problems/reverse-linked-list/
 // 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
 import Foundation
 
@@ -12,17 +13,19 @@ class ListNode {
     }
 }
 
-// 1 2 3 4 5     5 4 3 2 1
+// 迭代法
 func reverseList(_ head: ListNode?) -> ListNode? {
-        guard var head = head else {
-            return nil
-        }
-        var preNode: ListNode? = nil
-        while var nextNode = head.next {
-            head.next = preNode
-            preNode = head
-            head = nextNode
-        }
+    guard var head = head else { return nil }
+    var preNode: ListNode? = nil
+    while var nextNode = head.next {
         head.next = preNode
-        return head
+        preNode = head
+        head = nextNode
+    }
+    head.next = preNode
+    return head
 }
+
+
+//TODO: 递归法
+
