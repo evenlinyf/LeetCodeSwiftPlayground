0e9bf0b0e1b1a3b4389f1d0215fa34d080cbc377
EvenLin <evenlinyf@gmail.com>
2022-06-30 15:36:54 +0800

    linked-list-cycle

---
+++ LeetCode-Swift.playground/Pages/114-linked-list-cycle.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+//: [Previous](@previous)
+//https://leetcode.cn/problems/linked-list-cycle/
+import Foundation
+
+class ListNode {
+    
+    var val: Int
+    var next: ListNode?
+    
+    init(val: Int = 0, next: ListNode? = nil) {
+        self.val = val
+        self.next = next
+    }
+}
+
+class LinkedListCycle {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        guard var head = head else { return false }
+        var nodes: [ListNode] = [head]
+        while let next = head.next {
+            if nodes.contains(where: {$0 === next} ) { return true }
+            nodes.append(next)
+            head = next
+        }
+        return false
+    }
+    
+    func hasCycleRabbitTurtle(_ head: ListNode?) -> Bool {
+        var turtle: ListNode? = head
+        var rabbit: ListNode? = head?.next
+        while rabbit != nil {
+            if turtle === rabbit {
+                return true
+            }
+            turtle = turtle?.next
+            rabbit = rabbit?.next?.next
+        }
+        return false
+    }
+    
+}
